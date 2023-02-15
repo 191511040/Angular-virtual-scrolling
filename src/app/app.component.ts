9292a91bc1b969217e03cd6764aa303e50009732
saijoti123 <saijoti123@yahoo.com>
2023-02-15 12:17:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items: any = [];
+  viewportHeight = 500;
   title = 'angularvirtualscrolling';
+  constructor() {
+    // Generate a large array of items
+    for (let i = 0; i < 100000; i++) {
+      this.items.push(`Item ${i}`);
+    }
+  }
 }
